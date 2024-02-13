7ad21e47c8ed74205c226eddd191d9bb42c9c038
Moonjunwoong <myc0611@naver.com>
2024-02-13 23:56:30 +0900

    feat : 기능 구현중

--- src/main/java/com/gml/semi_project/Controller/memberController.java
+++
@@ -1,4 +0,0 @@
-package com.gml.semi_project.Controller;
-
-public class memberController {
-}
--- src/main/java/com/gml/semi_project/Entity/User.java
+++ src/main/java/com/gml/semi_project/Entity/User.java
@@ -6,8 +6,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -21,8 +28,8 @@ public class User {
     private Long id;
 
     private String memberId; // 아이디
-    private String memberPassword; // 비밀번호
-    private String memberNickname; // 닉네임
+    private String Password; // 비밀번호
+    private String Nickname; // 닉네임
     private LocalDateTime createAt; // 만든 시간
     private Integer receivedWarningCnt; // 받은 신고 수
     private Integer receivedLikeCnt; // 받은 좋아요 수
@@ -30,7 +37,6 @@ public class User {
     @Enumerated(EnumType.STRING)
     private UserRole userRole; // 권한
 
-    /*
     @OneToMany(mappedBy = "user", orphanRemoval = true)
     private List<Board> boards;
 
@@ -39,6 +45,34 @@ public class User {
 
     @OneToMany(mappedBy = "user", orphanRemoval = true)
     private List<Comment> comments;
-     */
+
+    public void rankUp(UserRole userRole, Authentication auth) {
+        this.userRole = userRole;
+
+        // 현재 저장되어 있는 Authentication 수정 => 재로그인 하지 않아도 권한 수정 되기 위함
+        List<GrantedAuthority> updatedAuthorities = new ArrayList<>();
+        updatedAuthorities.add(new SimpleGrantedAuthority(userRole.name()));
+        Authentication newAuth = new UsernamePasswordAuthenticationToken(auth.getPrincipal(), auth.getCredentials(), updatedAuthorities);
+        SecurityContextHolder.getContext().setAuthentication(newAuth);
+    }
+
+    public void likeChange(Integer receivedLikeCnt) {
+        this.receivedLikeCnt = receivedLikeCnt;
+        //좋아요 10개 이상 받으면 vip
+        if (this.receivedLikeCnt >= 10 && this.userRole.equals(UserRole.NEWBIE)) {
+            this.userRole = UserRole.VIP;
+        }
+    }
+
+    public void edit(String newPassword, String newNickname) {
+        this.password = newPassword;
+        this.nickname = newNickname;
+    }
+
+    public void changeRole() {
+        if (userRole.equals(UserRole.NEWBIE)) userRole = UserRole.VIP;
+        else if (userRole.equals(UserRole.VIP)) userRole = UserRole.BLACKLIST;
+        else if (userRole.equals(UserRole.BLACKLIST)) userRole = UserRole.NEWBIE;
+    }
 
 }
